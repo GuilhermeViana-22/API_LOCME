589cf4b1d2e5ecb8ad9a06e661614f0c90968653
GuilhermeViana-22 <gguicido.viana@gmail.com>
2024-08-01 03:25:53 -0300

    ajustes

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,20 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 // Rotas de Autenticação
-Route::get('/', [AuthController::class, 'teste']);
+Route::prefix('api')->group(function () {
+    // Rota de teste para verificar se a API está funcionando
+    Route::get('/', [AuthController::class, 'teste'])->name('api.test');
 
-Route::post('/register', [AuthController::class, 'register'])->name('.register');
-Route::post('/login', [AuthController::class, 'login'])->name('.login');
-Route::post('/verifycode', [VerificationCodeController::class, 'verifyCode'])->name('.verifycode');
-Route::post('/mailverify', [AuthController::class, 'mailVerify'])->name('.mailverify');
-Route::post('/reset', [AuthController::class, 'reset'])->name('.reset');
+    // Rotas públicas para autenticação e gerenciamento de conta
+    Route::post('register', [AuthController::class, 'register'])->name('api.register');
+    Route::post('login', [AuthController::class, 'login'])->name('api.login');
+    Route::post('verifycode', [VerificationCodeController::class, 'verifyCode'])->name('api.verifycode');
+    Route::post('mailverify', [AuthController::class, 'mailVerify'])->name('api.mailverify');
+    Route::post('reset', [AuthController::class, 'reset'])->name('api.reset');
 
-// Grupo de rotas protegidas por autenticação
-Route::middleware('auth:api')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout'])->name('.logout');;
-    Route::get('me', [AuthController::class, 'me'])->name('.me');;
+    // Rotas protegidas por autenticação
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout'])->name('api.logout');
+        Route::get('me', [AuthController::class, 'me'])->name('api.me');
+    });
 });
