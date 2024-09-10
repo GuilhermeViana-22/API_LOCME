36c30105f02711265de097a8c9832aa923640540
Guilherme Augusto dos Santos Viana <guilherme.gasv@policiacientifica.sp.gov.br>
2024-09-10 12:12:09 -0300

    ajustes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ActivityLogController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\VerificationCodeController;
-use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
 
+
+Route::post('/register', [AuthController::class, 'register'])->name('api.register');
+Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+Route::post('/verifycode', [VerificationCodeController::class, 'verifyCode'])->name('api.verifycode');
+Route::post('/mailverify', [AuthController::class, 'mailVerify'])->name('api.mailverify');
+Route::post('/reset', [AuthController::class, 'reset'])->name('api.reset');
 
 // Rotas públicas para autenticação e gerenciamento de conta com Rate Limiting
 Route::middleware('throttle:alert:10,1')->group(function () {
-    Route::post('/register', [AuthController::class, 'register'])->name('api.register');
-    Route::post('/login', [AuthController::class, 'login'])->name('api.login');
-    Route::post('/verifycode', [VerificationCodeController::class, 'verifyCode'])->name('api.verifycode');
-    Route::post('/mailverify', [AuthController::class, 'mailVerify'])->name('api.mailverify');
-    Route::post('/reset', [AuthController::class, 'reset'])->name('api.reset');
+
 });
 
 Route::get('/me', [AuthController::class, 'me'])->name('me');
