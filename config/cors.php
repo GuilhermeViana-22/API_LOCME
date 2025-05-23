108c00393b43c0d6a23837bbe7ea35b63cea4c91
GuilhermeViana-22 <gguicido.viana@gmail.com>
2025-05-23 11:57:47 -0300

    ajustes

--- config/cors.php
+++ config/cors.php
@@ -1,23 +1,12 @@
 <?php
 
 return [
-
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'https://bobflow-client.vercel.app',
-    ],
-
+    'allowed_origins' => ['*'], // Específico, não use *
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
+    'supports_credentials' => true, // Isso é crucial
 ];
